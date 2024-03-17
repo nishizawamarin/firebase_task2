33f985e4d5260825326f771292efd39bf695f16f
nishizawamarin <777marin0709@ezweb.ne.jp>
2024-03-17 16:45:09 +0900

    pet

--- lib/animal_type.dart
+++ lib/animal_type.dart
@@ -7,39 +7,44 @@ import 'package:firebase_core/firebase_core.dart';
 class Amimaltype extends StatefulWidget {
   const Amimaltype({super.key});
 
-
   @override
   State<Amimaltype> createState() => _AmimaltypeState();
 }
 
-
 class _AmimaltypeState extends State<Amimaltype> {
-
-
   late Stream<QuerySnapshot> petStream;
   final FirestoreService firestoreService = FirestoreService();
 
+  late Stream<QuerySnapshot> _petStream =
+      FirebaseFirestore.instance.collection('pet').snapshots();
 
-  late Stream<QuerySnapshot> _petStream = FirebaseFirestore.instance.collection('pet').snapshots();
+  late Stream<QuerySnapshot> _dogpetStream = FirebaseFirestore.instance
+      .collection('pet')
+      .orderBy('age', descending: false)
+      .where('type', isEqualTo: '犬')
+      .snapshots();
 
-  late Stream<QuerySnapshot> _dogpetStream = FirebaseFirestore.instance.collection('pet').
-  orderBy('age', descending: false).where('type', isEqualTo: '犬').snapshots();
+  late Stream<QuerySnapshot> _catpetStream = FirebaseFirestore.instance
+      .collection('pet')
+      .where('type', isEqualTo: '猫')
+      .snapshots();
 
-  late Stream<QuerySnapshot> _catpetStream = FirebaseFirestore.instance.collection('pet').
-  where('type', isEqualTo: '猫').snapshots();
+  late Stream<QuerySnapshot> _ageascendingpetStream = FirebaseFirestore.instance
+      .collection('pet')
+      .orderBy('age', descending: false)
+      .snapshots();
 
-  late Stream<QuerySnapshot> _ageascendingpetStream = FirebaseFirestore.instance.collection('pet').
-  orderBy('age', descending: false).snapshots();
-
-  late Stream<QuerySnapshot> _agedescendingpetStream = FirebaseFirestore.instance.collection('pet').
-  orderBy('age', descending: true).snapshots();
+  late Stream<QuerySnapshot> _agedescendingpetStream = FirebaseFirestore
+      .instance
+      .collection('pet')
+      .orderBy('age', descending: true)
+      .snapshots();
 
   @override
   void initState() {
     super.initState();
     petStream = FirebaseFirestore.instance.collection('pet').snapshots();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -48,74 +53,80 @@ class _AmimaltypeState extends State<Amimaltype> {
         actions: <Widget>[
           PopupMenuButton(
               onSelected: (value) {
-    switch (value) {
-    case 'dog' : setState(() {
-    switch (value) {
-    case 'dog':
-    _petStream = _dogpetStream;
-    break;
-    case 'cat':
-    _petStream = _catpetStream;
-    break;
-    case 'ascending':
-    _petStream = _ageascendingpetStream;
-    break;
-    case 'descending':
-    _petStream = _agedescendingpetStream;
-    break;
-    }
-    });
-    }
-    },
-    itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-    const PopupMenuItem<String>(
-    value: 'dog',
-    child: Text('犬のみ'),
-    ),
-    const PopupMenuItem<String>(
-    value: 'cat',
-    child: Text('猫のみ'),
-    ),
-    const PopupMenuItem<String>(
-    value: 'ascending',
-    child: Text('年齢：昇順'),
-    ),
-    const PopupMenuItem<String>(
-    value: 'descending',
-    child: Text('年齢：降順'),
-    ),
-    ]
-    ),
+                switch (value) {
+                  case 'dog':
+                    setState(() {
+                      switch (value) {
+                        case 'dog':
+                          _petStream = _dogpetStream;
+                          break;
+                        case 'cat':
+                          _petStream = _catpetStream;
+                          break;
+                        case 'ascending':
+                          _petStream = _ageascendingpetStream;
+                          break;
+                        case 'descending':
+                          _petStream = _agedescendingpetStream;
+                          break;
+                      }
+                    });
+                }
+              },
+              itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                    const PopupMenuItem<String>(
+                      value: 'dog',
+                      child: Text('犬のみ'),
+                    ),
+                    const PopupMenuItem<String>(
+                      value: 'cat',
+                      child: Text('猫のみ'),
+                    ),
+                    const PopupMenuItem<String>(
+                      value: 'ascending',
+                      child: Text('年齢：昇順'),
+                    ),
+                    const PopupMenuItem<String>(
+                      value: 'descending',
+                      child: Text('年齢：降順'),
+                    ),
+                  ]),
         ],
       ),
       body: StreamBuilder<QuerySnapshot>(
-    stream: _petStream,
-    builder: (context, snapshot) {
-    if (snapshot.hasData) {
-    List<DocumentSnapshot> petsData = snapshot.data!.docs;
+        stream: _petStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<DocumentSnapshot> petsData = snapshot.data!.docs;
 
-    return ListView.separated(
-        itemCount: petsData.length,
-        itemBuilder: (BuildContext context, int index) {
-          Map<String,dynamic> petData = petsData[index].data()! as Map<String, dynamic>;
-          return ListTile(
-            title: Text('名前：${petData['name']}''品種：${petData['breeds']}'
-                '性別：${petData['gender']}''年齢：${petData['age']}'),);
+            return ListView.separated(
+              itemCount: petsData.length,
+              itemBuilder: (BuildContext context, int index) {
+                Map<String, dynamic> petData =
+                    petsData[index].data()! as Map<String, dynamic>;
+                return ListTile(
+                  title: Text('名前：${petData['name']}'
+                      '品種：${petData['breeds']}'
+                      '性別：${petData['gender']}'
+                      '年齢：${petData['age']}'),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          ;
         },
-        separatorBuilder: (BuildContext context, int index) => const Divider(),
-       );
-    }else{
-      return const Center(
-        child: CircularProgressIndicator(),
-      );
-    };
-    },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.of(context).push(MaterialPageRoute(
-        builder: (context) => const TypeInput())),
-        child: const Icon(Icons.add),),
+        onPressed: () => Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => const TypeInput())),
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
-
--- lib/type_input.dart
+++ lib/type_input.dart
@@ -6,16 +6,18 @@ import 'package:firebase_task2/firestore_service.dart';
 import 'package:firebase_task2/animal_type.dart';
 
 class TypeInput extends StatefulWidget {
-  const TypeInput({super.key});
+  const TypeInput({Key? key}) : super(key: key);
 
   @override
   State<TypeInput> createState() => _TypeInputState();
 }
 
 enum TypeList { dog, cat }
+
 TypeList _type = TypeList.dog;
 
 enum GenderList { male, female }
+
 GenderList _gender = GenderList.male;
 
 class _TypeInputState extends State<TypeInput> {
@@ -23,6 +25,7 @@ class _TypeInputState extends State<TypeInput> {
   final TextEditingController _breedscontroller = TextEditingController();
   final TextEditingController _agecontroller = TextEditingController();
 
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   final FirestoreService = FirebaseFirestore.instance;
 
@@ -70,26 +73,30 @@ class _TypeInputState extends State<TypeInput> {
           Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              RadioListTile(
-                  title: const Text('犬'),
-                  controlAffinity: ListTileControlAffinity.leading,
-                  value: TypeList.dog,
-                  groupValue: _type,
-                  onChanged: (value) {
-                    setState(() {
-                      _type = value!;
-                    });
-                  }),
-              RadioListTile(
-                  title: const Text('猫'),
-                  controlAffinity: ListTileControlAffinity.leading,
-                  value: TypeList.cat,
-                  groupValue: _type,
-                  onChanged: (value) {
-                    setState(() {
-                      _type = value!;
-                    });
-                  }),
+              Expanded(
+                child: RadioListTile(
+                    title: const Text('犬'),
+                    controlAffinity: ListTileControlAffinity.leading,
+                    value: TypeList.dog,
+                    groupValue: _type,
+                    onChanged: (value) {
+                      setState(() {
+                        _type = value!;
+                      });
+                    }),
+              ),
+              Expanded(
+                child: RadioListTile(
+                    title: const Text('猫'),
+                    controlAffinity: ListTileControlAffinity.leading,
+                    value: TypeList.cat,
+                    groupValue: _type,
+                    onChanged: (value) {
+                      setState(() {
+                        _type = value!;
+                      });
+                    }),
+              ),
             ],
           ),
           const SizedBox(
@@ -97,38 +104,39 @@ class _TypeInputState extends State<TypeInput> {
           ),
           TextField(
             controller: _breedscontroller,
-            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
             decoration: const InputDecoration(
               border: OutlineInputBorder(),
               labelText: '品種',
             ),
           ),
-
           const SizedBox(
             height: 10,
           ),
           Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              RadioListTile(
-                  title: const Text('オス'),
-                  controlAffinity: ListTileControlAffinity.leading,
-                  value: GenderList.male,
-                  groupValue: _gender,
-                  onChanged: (value) {
-                    _gender = value!;
-                  }),
-              RadioListTile(
-                  title: const Text('メス'),
-                  controlAffinity: ListTileControlAffinity.leading,
-                  value: GenderList.female,
-                  groupValue: _gender,
-                  onChanged: (value) {
-                    _gender = value!;
-                  }),
+              Expanded(
+                child: RadioListTile(
+                    title: const Text('オス'),
+                    controlAffinity: ListTileControlAffinity.leading,
+                    value: GenderList.male,
+                    groupValue: _gender,
+                    onChanged: (value) {
+                      _gender = value!;
+                    }),
+              ),
+              Expanded(
+                child: RadioListTile(
+                    title: const Text('メス'),
+                    controlAffinity: ListTileControlAffinity.leading,
+                    value: GenderList.female,
+                    groupValue: _gender,
+                    onChanged: (value) {
+                      _gender = value!;
+                    }),
+              ),
             ],
           ),
-
           const SizedBox(
             height: 10,
           ),
@@ -140,18 +148,25 @@ class _TypeInputState extends State<TypeInput> {
               labelText: '年齢',
             ),
           ),
-          const SizedBox(height: 10,),
-
+          const SizedBox(
+            height: 10,
+          ),
           ElevatedButton(
             child: const Text('登録'),
             onPressed: () async {
-              await FirebaseFirestore.instance.collection('pet').add({
-                'name':_namecontroller,
+              await firestore.collection('pets').add({
+                'name': _namecontroller.text,
                 'type': _type.toString().split('.').last,
-                'breeds':_breedscontroller,
-                'gender':_gender.toString().split('.').last,
-                'age':_agecontroller,
-
+                'breeds': _breedscontroller.text,
+                'gender': _gender.toString().split('.').last,
+                'age': int.parse(_agecontroller.text),
+              });
+              _namecontroller.clear();
+              _breedscontroller.clear();
+              _agecontroller.clear();
+              setState(() {
+                _type = TypeList.dog;
+                _gender = GenderList.male;
               });
             },
           ),
